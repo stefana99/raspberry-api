035608794083be73119b021ca2ccd8027fe4b66e
Stefan Arngrimsson <stefanjohann@wow.is>
2017-07-18 22:40:31 +0000

    controller

--- Controllers/TempraturesController.cs
+++ Controllers/TempraturesController.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using raspberry_api.Models;
 
 namespace raspberry_api.Controllers
 {
     [Route("api/[controller]")]
     public class TempraturesController : Controller
     {
+        private readonly TempraturesContext _context;
+        public TempraturesController(TempraturesContext context)
+        {
+            _context = context;
+        }
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Reading> Get()
         {
-            return new string[] { "value1", "value2" };
+            return await _context.Readings.ToList();
         }
     }
 }
